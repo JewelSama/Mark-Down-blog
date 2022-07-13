b1629b0690432c9258381c038f8434cf11b56654
Jewel Osafile <osafilejewel@gmail.com>
2022-07-13 10:38:30 +0100

    ejs setup

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express')
 
 const PORT = process.env.PORT || 5000
 const app = express()
-app.set('view-engine', 'ejs')
+
+app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('index')
 } )
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
