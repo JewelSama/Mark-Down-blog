cab7122ea8e571964169cbeb0b23d6c3c76d6f7a
Jewel Osafile <osafilejewel@gmail.com>
2022-07-13 10:31:32 +0100

    views

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 5000
+const app = express()
+app.set('view-engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+} )
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
